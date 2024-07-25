1760f775ab20d47152ca4526865c1ca67a16df35
ColinVanderMeer <colinvandermeer@gmail.com>
2024-07-25 17:36:01 -0400

    Add sprig definition

---
+++ 32blit-pico/board/hackclub_sprig/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define BUTTON_LEFT_PIN  6
+#define BUTTON_RIGHT_PIN 8
+#define BUTTON_UP_PIN    5
+#define BUTTON_DOWN_PIN  7
+#define BUTTON_A_PIN     15
+#define BUTTON_B_PIN     14
+#define BUTTON_X_PIN     12
+#define BUTTON_Y_PIN     13
+
+#define DISPLAY_WIDTH 160
+
+#define LCD_CS_PIN 20
+#define LCD_DC_PIN 22
+#define LCD_SCK_PIN 18
+#define LCD_MOSI_PIN 19
+#define LCD_BACKLIGHT_PIN 17
+#define LCD_RESET_PIN 26
+
